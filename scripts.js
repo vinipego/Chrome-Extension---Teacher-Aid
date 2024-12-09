7c9c71b4369f774e9a700d0a90e8bf84c1e86a74
vinipego <vinicius.pego.pereira@gmail.com>
2024-12-09 04:05:58 -0300

    finished extension

--- scripts.js
+++ scripts.js
@@ -8,6 +8,11 @@ const fiveMinuteShortcut = document.querySelector("#five-minute-shortcut");
 const oneMinuteShortcut = document.querySelector("#one-minute-shortcut");
 const halfMinuteShortcut = document.querySelector("#half-minute-shortcut");
 const predefinedTimesContainer = document.querySelector(".predefined-times");
+const notesArea = document.querySelector("#notes-area");
+const saveNotes = document.querySelector("#save-notes-button");
+const resumeNotes = document.querySelector("#resume-notes-button");
+
+
 // Select the progress circle
 const progressCircle = document.querySelector(".progress-ring__circle");
 
@@ -22,8 +27,15 @@ progressCircle.style.strokeDashoffset = `${circumference}`;
 let countdown; // Reference for setInterval
 let remainingTime = 0; // Total time in seconds
 let isRunning = false; // Timer state
+let paused = false; // Stop/Resume btns state
 let initialTime = 0; // Store the original user input in seconds
 let audio = undefined; // Declare a variable to hold the audio instance
+
+// review
+if(localStorage.getItem("savedNotes")){
+  resumeNotes.hidden = false;
+}
+
 
 timerInput.addEventListener("input", (e) => {
   let value = e.target.value;
@@ -59,7 +71,7 @@ timerInput.addEventListener("input", (e) => {
 
   // Update the input field value
   e.target.value = value;
-}); // end of timerInput event listener
+});
 
 // The timerInput will react to the enter key as well
 timerInput.addEventListener("keydown", (e) => {
@@ -69,7 +81,7 @@ timerInput.addEventListener("keydown", (e) => {
 });
 
 // Start the timer
-playButton.addEventListener("click", () => {
+playButton.addEventListener("click", () => {  
   let timerValue = timerInput.value.trim(); // Get input value and remove extra spaces
 
   // If input has no colon, interpret it as seconds
@@ -87,7 +99,7 @@ playButton.addEventListener("click", () => {
   // Prevent invalid or empty input
   if (isNaN(remainingTime) || remainingTime <= 0) {
     timerInput.value = "";
-    showTooltip("Please enter a valid time!");
+    showTooltip("Please enter a valid time!", 2000);
 
     return; // Prevents disabling the input
   }
@@ -99,23 +111,24 @@ playButton.addEventListener("click", () => {
   stopResumeButton.hidden = false;
   resetButton.hidden = false;
 
-  toggleShortcutButtons(false); 
-  // Disable shortcut buttons
-  // console.log(tenMinuteShortcut.disabled)
   // Start the countdown
   startCountdown();
+  greyShortcutButtons();
+  
 });
 
 stopResumeButton.addEventListener("click", () => {
   if (isRunning) {
     // Pause the timer
     clearInterval(countdown); // Stop the countdown
-    isRunning = false; // Update state
+    isRunning = false;
+    paused = true;
     stopResumeButton.textContent = "Resume"; // Change button text
   } else {
     // Resume the timer
     startCountdown(); // Restart the countdown
     stopResumeButton.textContent = "Pause"; // Change button text
+    paused = false;
   }
 });
 
@@ -123,6 +136,7 @@ resetButton.addEventListener("click", () => {
   // Stop the timer
   clearInterval(countdown);
   isRunning = false;
+  paused = false;
   remainingTime = initialTime;
 
   // Reset the timer display
@@ -133,10 +147,10 @@ resetButton.addEventListener("click", () => {
 
   // Reset the input field
   updateTimerDisplay(remainingTime); // Reset to the last input value
+  greyShortcutButtons();
 
   // Reset the progress circle
   progressCircle.style.strokeDashoffset = `${circumference}`;
-  toggleShortcutButtons(true); // Enable shortcut buttons
   if (audio && !audio.paused) {
     audio.pause(); // Pause the audio
     audio.currentTime = 0; // Reset playback to the start
@@ -145,41 +159,47 @@ resetButton.addEventListener("click", () => {
 });
 
 tenMinuteShortcut.addEventListener("click", () => {
-  timerInput.value = "10:00";
-});
-
-fiveMinuteShortcut.addEventListener("click", () => {
-  timerInput.value = "05:00";
-});
-
-oneMinuteShortcut.addEventListener("click", () => {
-  timerInput.value = "01:00";
-});
-
-halfMinuteShortcut.addEventListener("click", () => {
-  timerInput.value = "00:30";
-});
-
-predefinedTimesContainer.addEventListener("click", (e) => {
-  const clickedButton = e.target;
-  // console.log("Button clicked:", clickedButton.id);
-
-  if (clickedButton.tagName === "BUTTON") {
-    // console.log("Is button disabled?", clickedButton.disabled);
-
-    if (clickedButton.disabled) {
-      // console.log("Showing tooltip for disabled button");
-      showTooltip("Timer is already running. Shortcuts are disabled!");
-      return;
-    }
-
-    // Ensure this block executes only for enabled buttons
-    // console.log("Shortcut button is enabled");
+  if (!isRunning && !paused) {
+    timerInput.value = "10:00";
   }
 });
 
+fiveMinuteShortcut.addEventListener("click", () => {
+  if (!isRunning && !paused) {
+    timerInput.value = "05:00";
+  }
+});
+
+oneMinuteShortcut.addEventListener("click", () => {
+  if (!isRunning && !paused) {
+    timerInput.value = "01:00";
+  }
+});
+
+halfMinuteShortcut.addEventListener("click", () => {
+  if (!isRunning && !paused) {
+    timerInput.value = "00:30";
+  }
+});
+
+saveNotes.addEventListener("click", () => {
+
+  if(notesArea.value){
+    localStorage.setItem("savedNotes", notesArea.value);
+    console.log(localStorage.getItem("savedNotes"));
+    resumeNotes.hidden = false;
+  }else{
+    localStorage.removeItem("savedNotes");
+  }
+});
+
+resumeNotes.addEventListener("click", () =>{
+notesArea.textContent = localStorage.getItem("savedNotes");
+});
+
+
 // Show the tooltip
-function showTooltip(message) {
+function showTooltip(message, delay) {
   // console.log("Tooltip message:", message);
   tooltip.textContent = message;
   tooltip.hidden = false;
@@ -188,7 +208,7 @@ function showTooltip(message) {
   setTimeout(() => {
     // console.log("Hiding tooltip");
     tooltip.hidden = true;
-  }, 3000);
+  }, delay);
 }
 
 function startCountdown() {
@@ -244,15 +264,16 @@ function updateProgress(remainingTime, initialTime) {
   progressCircle.style.strokeDashoffset = offset; // Apply the offset
 }
 
-function toggleShortcutButtons(state) {
-  const shortcutButtons = [
-    tenMinuteShortcut,
-    fiveMinuteShortcut,
-    oneMinuteShortcut,
-    halfMinuteShortcut,
-  ];
-  shortcutButtons.forEach((button) => {
-    button.disabled = !state; // Enable if `state` is true, disable if false
-  });
+function greyShortcutButtons(yes) {
+  if (isRunning) {
+    tenMinuteShortcut.classList.add("grey-out");
+    fiveMinuteShortcut.classList.add("grey-out");
+    oneMinuteShortcut.classList.add("grey-out");
+    halfMinuteShortcut.classList.add("grey-out");
+  } else {
+    tenMinuteShortcut.classList.remove("grey-out");
+    fiveMinuteShortcut.classList.remove("grey-out");
+    oneMinuteShortcut.classList.remove("grey-out");
+    halfMinuteShortcut.classList.remove("grey-out");
+  }
 }
-
